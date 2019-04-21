d1741c52ebe4994dc2e9a79ad8b58e254a47a034
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-04-21 15:24:53 +0700

    ngrx/store example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { counterReducer } from './counter/counter.reducer';
+import { CounterComponent } from './counter/counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ count: counterReducer }),
     AppRoutingModule
   ],
   providers: [],
